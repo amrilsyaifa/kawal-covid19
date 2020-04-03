627aff79e38488d090706469d930460234b33e07
amril <amrilsyaifa@gmail.com>
2020-04-03 22:57:36 +0700

    Updates

---
+++ precache-manifest.7fa89f1e0cd4806626d24294aa2dc933.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298b8cc68bc048c14de3955b63994d43",
+    "url": "/kawal-covid19/index.html"
+  },
+  {
+    "revision": "1ade97d2d3526795d556",
+    "url": "/kawal-covid19/static/js/2.1c254b79.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/kawal-covid19/static/js/2.1c254b79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a9309e646af6522be6",
+    "url": "/kawal-covid19/static/js/main.5dbe21a4.chunk.js"
+  },
+  {
+    "revision": "c25db152f782b5fe0308",
+    "url": "/kawal-covid19/static/js/runtime-main.404487ff.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.70989c91ddd6a74694b2dd045a562969.js"
+  "/kawal-covid19/precache-manifest.7fa89f1e0cd4806626d24294aa2dc933.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kawal-covid19/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
